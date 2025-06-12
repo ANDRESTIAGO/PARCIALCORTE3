a4cb8146b56cf784c9995b194b34a2ba51a85871
labsis <labsis@debian.FI-LabSistemas.ucatolica.loc>
2025-06-12 04:12:52 -0500

    Creacion de modelos

--- models.py
+++ models.py
@@ -2,23 +2,27 @@ from pydantic import BaseModel
 from typing import Optional
 from sqlmodel import SQLModel, Field
 
-class Componente(SQLModel):
+class usuario(SQLModel):
     nombre: str
-    tipo: str
-    marca: str
-    modelo: str
-class ComponenteConId(Componente):
+    id_compra: int
+    edad: str
+    sexo: str
+class UsuarioConId(usuario):
     id: int
 
-class ComponenteActualizado(SQLModel):
+class mascota(SQLModel):
     nombre: Optional[str] = Field(None, min_length=2, max_length=50)
     tipo: Optional[str] = Field(None, min_length=2, max_length=30)
     marca: Optional[str] = Field(None, min_length=2, max_length=30)
     modelo: Optional[str] = Field(None, min_length=2, max_length=30)
     
-class Orden(SQLModel, table=True):
+class MascotaConId(mascota):
+    id: int
+
+class boleto(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    componente_id: int
-    componente_nombre: str
-    componente_tipo: str
-    componente_marca: str
+    Ciudad_origen: int
+    Ciudad_destino: str
+    fecha:  str
+    disponibilidad: str
+    Snack: str
